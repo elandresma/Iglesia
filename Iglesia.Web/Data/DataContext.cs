2356cf198e31a2f2e933340009adb6071aabd0c9
Andrés Marín Hoyos <Amarinh@intergrupo.com>
2020-08-27 00:25:35 -0500

    Creaate the architecture of the project and the database

---
+++ Iglesia.Web/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Iglesia.Common.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Iglesia.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Region> Regions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Region>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+    }
+}
+
